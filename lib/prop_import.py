164549ec72a2ecaf8d867f2bbe947d9084fe07e0
Kate Polley <klpolley@syr.edu>
2020-11-28 20:03:47 -0500

    wiki prop import prep

--- lib/prop_import.py
+++ lib/prop_import.py
@@ -1,9 +1,9 @@
 from selenium import webdriver
-import csv
+import csv, json
 
 def add_new_properties():
-    driver_location = "H:/"
-    driver = webdriver.Firefox(driver_location)
+    #driver_location = "H:/"
+    #driver = webdriver.Firefox(driver_location)
 
     label = '//*[@id="ooui-php-3"]'
     desc = '//*[@id="ooui-php-4"]'
@@ -11,19 +11,27 @@ def add_new_properties():
     submit = '/html/body/div[3]/div[3]/div[4]/div[4]/form/fieldset/div/div/div[6]/span/button'
     string = '/html/body/div[5]/div[2]/div[12]/span[3]'
 
-    with open("../data/wiki_properties/props_for_import.csv") as props_file:
+    next_q = 24
+    output = {}
+
+    with open("data/wiki_properties/props_for_import.csv") as props_file:
         reader = csv.reader(props_file)
         for row in reader:
-            driver.get('http://linkeddata.ischool.syr.edu/mediawiki/index.php/Special:NewProperty')
+            #driver.get('http://linkeddata.ischool.syr.edu/mediawiki/index.php/Special:NewProperty')
 
             prop_label = row[0]
             description = row[1]
 
-            driver.find_element_by_xpath(label).send_keys(prop_label)
-            driver.find_element_by_xpath(desc).send_keys(description)
+            output[prop_label] = next_q
+            next_q += 1
 
-            driver.find_element_by_xpath(type).click()
-            driver.find_element_by_xpath(string).click()
+            # driver.find_element_by_xpath(label).send_keys(prop_label)
+            # driver.find_element_by_xpath(desc).send_keys(description)
+            #
+            # driver.find_element_by_xpath(type).click()
+            # driver.find_element_by_xpath(string).click()
+            #
+            # driver.find_element_by_xpath(submit).click()
 
-            driver.find_element_by_xpath(submit).click()
-
+    with open("data/wiki_props.json", "w") as json_file:
+        json.dump(output, json_file)
