71102be9727a737f4f73757613a51a5b3f654ca2
Kate Polley <klpolley@syr.edu>
2020-11-29 10:50:51 -0500

    more koppel because it's easier than re-running everything

--- koppel.py
+++ koppel.py
@@ -1,4 +1,6 @@
 from wikidataintegrator import wdi_core, wdi_login
+from lib.wikibase_import import extract_wiki_statements
+from lib.json_read import json_to_dict
 
 #prep for wikibase connection
 mw_api_url = "http://linkeddata.ischool.syr.edu/mediawiki/api.php"
@@ -6,7 +8,15 @@ login_creds = wdi_login.WDLogin(user='Admin', pwd="metadata!master", mediawiki_a
 
 
 def fix_koppel():
+
+    file = "data/entities/people_edited.json"
+    people = json_to_dict(file)
+    koppel_dict = people.get("Ted Koppel")
+    wiki_dict = koppel_dict.get("wiki")
+
     item_statements = [wdi_core.WDItemID("Q290", prop_nr="P5")]
+    wiki_statements = extract_wiki_statements(wiki_dict)
+    item_statements.append(wiki_statements)
 
     # create the item
     wbPage = wdi_core.WDItemEngine(wd_item_id="Q10123", data=item_statements, mediawiki_api_url=mw_api_url)
